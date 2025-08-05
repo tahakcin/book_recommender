84a8f114b0aaf43ed4981f14862feb4d699e4029
tahakcin <muhammettahaakcin@gmail.com>
2025-08-05 16:39:45 +0300

    Initial commit

---
+++ gradio-dashboard.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+import gradio as gr
+from scripts.regsetup import description
+
+load_dotenv()
+
+books = pd.read_csv("books_with_emotions.csv")
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "cover-not-found.jpg",
+    books["large_thumbnail"]
+)
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+raw_documents = TextLoader("tagged_description.txt", encoding="utf-8").load()
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
+documents = text_splitter.split_documents(raw_documents)
+db_books = Chroma.from_documents(documents, embedding=embedding_model)
+
+
+def retrieve_semantic_recommendations(
+        query: str,
+        category: str = None,
+        tone: str = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16
+) -> pd.DataFrame:
+    recs = db_books.similarity_search(query, k=initial_top_k)
+    books_list = [int(recs.page_content.strip('"').split()[0]) for recs in recs]
+    filtered = books[books["isbn13"].isin(books_list)]
+    book_recs = filtered.head(final_top_k)
+
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_categories"] == category][:final_top_k]
+    else:
+        book_recs = book_recs[:final_top_k]
+
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="angry", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="sadness", ascending=False, inplace=True)
+
+    return book_recs
+
+def recommend_books(
+        query: str,
+        category: str = None,
+        tone: str = None
+):
+
+    recommendations = retrieve_semantic_recommendations(query, category, tone)
+    results = []
+
+    for _, row in recommendations.iterrows():
+        description = row["description"]
+        truncated_desc_split = description.split()
+        truncated_description = " ".join(truncated_desc_split[:30]) + "..."
+
+        authors_split = row["authors"].split(";")
+        if len(authors_split) == 2:
+            authors_str = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])} and {authors_split[-1]}"
+        else:
+            authors_str = row["authors"]
+
+        caption = f"{row["title"]} by {authors_str}: {truncated_description}"
+        results.append((row["large_thumbnail"], caption))
+    return results
+
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label="Please enter a description of a book:",
+                                placeholder="e.g., A story about forgiveness")
+        tone_dropdown = gr.Dropdown(choices=categories, label="Select an emotional tone:", value="All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label="Recommended books", columns=8, rows=2)
+
+    submit_button.click(fn=recommend_books,
+                        inputs=[user_query, tone_dropdown],
+                        outputs=output,
+    )
+
+    if __name__ == "__main__":
+        dashboard.launch()
